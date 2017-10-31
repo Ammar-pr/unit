23b368d7e925a08248344f2546fbae252f785d85
MSIGRENADY <cs.ammarn@gmail.com>
2017-10-30 23:56:08 -0800

    git commit 'add admin login  with heddin value =22'

---
+++ templates/admin_login.tpl
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="jqwidgets/jqwidgets/styles/jqx.base.css" type="text/css" />
+    <script type="text/javascript" src="jqwidgets/scripts/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="jqwidgets/jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="jqwidgets/jqwidgets/jqxcheckbox.js"></script>
+    <script type="text/javascript" src="jqwidgets/jqwidgets/jqxbuttons.js"></script>
+    <script type="text/javascript" src="jqwidgets/jqwidgets/jqxvalidator.js"></script>
+    <script type="text/javascript" src="jqwidgets/scripts/demos.js"></script>
+    <script type="text/javascript" src="jqwidgets/jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="jqwidgets/jqwidgets/jqxbuttons.js"></script>
+
+    <style type="text/css">
+        .demo-iframe {
+            border: none;
+            width: 600px;
+            height: 200px;
+            clear: both;
+            display: none;
+        }
+        .form #password, .form #username {
+            height: 24px;
+            margin-top: 5px;
+            width: 150px;
+        }
+        .form #rememberme {
+            margin-top: 5px;
+            margin-bottom: 8px;
+        }
+        .prompt {
+            margin-top: 10px; font-size: 10px;
+        }
+    </style>
+</head>
+<body>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        // Create Link Button.
+        $("#jqxButton").jqxLinkButton({ width: '150', height: '25' });
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        $("#username, #password").addClass('jqx-input');
+        if (theme != '') {
+            $("#username, #password").addClass('jqx-input-' + theme);
+        }
+        // add validation rules.
+        $('#form').jqxValidator({
+            rules: [
+                { input: '#username', message: 'Username is required!', action: 'keyup, blur', rule: 'required' },
+                { input: '#username', message: 'Your username must start with a letter!', action: 'keyup, blur', rule: 'startWithLetter' },
+                { input: '#username', message: 'Your username must be between 3 and 12 characters!', action: 'keyup, blur', rule: 'length=1,12' },
+                { input: '#password', message: 'Password is required!', action: 'keyup, blur', rule: 'required' },
+                { input: '#password', message: 'Your password must be between 4 and 12 characters!', action: 'keyup, blur', rule: 'length=1,12' }
+            ]
+
+        });
+        // validate form.
+        $("#loginButton").click(function () {
+            $('#form').jqxValidator('validate');
+        });
+        $("#form").on('validationSuccess', function () {
+            $("#form-iframe").fadeIn('fast');
+        });
+    });
+</script>
+<div style="height: 219px; display: block; font-size: 13px; font-family: Verdana;">
+    <form class="form" id="form" target="form-iframe" method="post" action="login.php" style="width: 650px;">
+        <div>
+            <h2>Login Demo</h2>
+        </div>
+        <label>Username:</label>
+        <div>
+            <input type="text" id="username" name="username" />
+        </div>
+        <label>Password:</label>
+        <div>
+            <input type="password" id="password" name="password" />
+        </div>
+
+        <div>
+            <input id="loginButton" type="submit" value="Login" /><br><br>
+        </div>
+
+        <div>
+            <a style='margin-left: 25px;' target="_blank" href="index.php" id='jqxButton'>go to main page</a>
+
+        </div>
+
+        <tr>
+            <td><input name="inputHidden" type="hidden" id="inputHidden" value=22  /></td>
+        </tr>
+
+
+    </form>
+
+</div>
+</body>
---
+++ templates_c/38eb6e3aa81efc541961b5bc0e5e4229c281d505_0.file.admin_login.tpl.php
@@ -0,0 +1,148 @@
+<?php
+/* Smarty version {Smarty::SMARTY_VERSION}, created on 2017-10-31 03:55:16
+  from "C:\xampp\htdocs\unit\templates\admin_login.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32-dev-22',
+  'unifunc' => 'content_59f82c64d90cf3_92512322',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '38eb6e3aa81efc541961b5bc0e5e4229c281d505' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\unit\\templates\\admin_login.tpl',
+      1 => 1509436513,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_59f82c64d90cf3_92512322 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="jqwidgets/jqwidgets/styles/jqx.base.css" type="text/css" />
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/scripts/jquery-1.11.1.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/jqwidgets/jqxcore.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/jqwidgets/jqxcheckbox.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/jqwidgets/jqxbuttons.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/jqwidgets/jqxvalidator.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/scripts/demos.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/jqwidgets/jqxcore.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="jqwidgets/jqwidgets/jqxbuttons.js"><?php echo '</script'; ?>
+>
+
+    <style type="text/css">
+        .demo-iframe {
+            border: none;
+            width: 600px;
+            height: 200px;
+            clear: both;
+            display: none;
+        }
+        .form #password, .form #username {
+            height: 24px;
+            margin-top: 5px;
+            width: 150px;
+        }
+        .form #rememberme {
+            margin-top: 5px;
+            margin-bottom: 8px;
+        }
+        .prompt {
+            margin-top: 10px; font-size: 10px;
+        }
+    </style>
+</head>
+<body>
+
+<?php echo '<script'; ?>
+ type="text/javascript">
+    $(document).ready(function () {
+        // Create Link Button.
+        $("#jqxButton").jqxLinkButton({ width: '150', height: '25' });
+    });
+<?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ type="text/javascript">
+    $(document).ready(function () {
+
+        $("#username, #password").addClass('jqx-input');
+        if (theme != '') {
+            $("#username, #password").addClass('jqx-input-' + theme);
+        }
+        // add validation rules.
+        $('#form').jqxValidator({
+            rules: [
+                { input: '#username', message: 'Username is required!', action: 'keyup, blur', rule: 'required' },
+                { input: '#username', message: 'Your username must start with a letter!', action: 'keyup, blur', rule: 'startWithLetter' },
+                { input: '#username', message: 'Your username must be between 3 and 12 characters!', action: 'keyup, blur', rule: 'length=1,12' },
+                { input: '#password', message: 'Password is required!', action: 'keyup, blur', rule: 'required' },
+                { input: '#password', message: 'Your password must be between 4 and 12 characters!', action: 'keyup, blur', rule: 'length=1,12' }
+            ]
+
+        });
+        // validate form.
+        $("#loginButton").click(function () {
+            $('#form').jqxValidator('validate');
+        });
+        $("#form").on('validationSuccess', function () {
+            $("#form-iframe").fadeIn('fast');
+        });
+    });
+<?php echo '</script'; ?>
+>
+<div style="height: 219px; display: block; font-size: 13px; font-family: Verdana;">
+    <form class="form" id="form" target="form-iframe" method="post" action="login.php" style="width: 650px;">
+        <div>
+            <h2>Login Demo</h2>
+        </div>
+        <label>Username:</label>
+        <div>
+            <input type="text" id="username" name="username" />
+        </div>
+        <label>Password:</label>
+        <div>
+            <input type="password" id="password" name="password" />
+        </div>
+
+        <div>
+            <input id="loginButton" type="submit" value="Login" /><br><br>
+        </div>
+
+        <div>
+            <a style='margin-left: 25px;' target="_blank" href="index.php" id='jqxButton'>go to main page</a>
+
+        </div>
+
+        <tr>
+            <td><input name="inputHidden" type="hidden" id="inputHidden" value=22  /></td>
+        </tr>
+
+
+    </form>
+
+</div>
+</body>
+<?php }
+}
