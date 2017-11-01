f0741ba2961ecae019b3affd9ad54525771e176a
MSIGRENADY <cs.ammarn@gmail.com>
2017-10-31 23:24:04 -0800

    git commit

--- deleting.php
+++ deleting.php
@@ -6,17 +6,29 @@
  * Time: 11:13 م
  */
 
+require_once ('units_requests.php');
 class deleting
 {
 
 
-  public function   check_before_delete () {
+    public function check_before_delete()
+    {
+
+        if (isset($_SESSION["username"]) & isset($_SESSION["password"]) & isset($_SESSION["authorization"])) {
+            if ($_SESSION["authorization"] == 22) {
+
+                intval($_GET['id']);
+
+                $unit_request_del = new units_requests();
+                $unit_request_del->delete(0);
 
 
+            }
 
 
-}
+        }
 
+    }
 }
 
 $del=new deleting();
