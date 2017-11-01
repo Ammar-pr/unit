b83e767f26de745f5980d4d46c020f6fe053be1e
MSIGRENADY <cs.ammarn@gmail.com>
2017-10-31 22:11:46 -0800

    git commit

--- grid_data.php
+++ grid_data.php
@@ -1,4 +1,6 @@
-<?php
+﻿<?php
+session_start();
+
 /**
  * Created by PhpStorm.
  * User: GRENADY
@@ -7,38 +9,39 @@
  */
 
 #Include the connect.php file
-include ('connect.php');
-// Connect to the database
+
+// Connasdfect to the database
 // connection String
-$mysqli = new mysqli('localhost', 'root', 'dwddwddwd', 'unit');
-/* check connection */
-if (mysqli_connect_errno())
-{
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
+
+
+if(isset($_SESSION["username"]) & isset($_SESSION["password"]) & $_SESSION["authorization"]==22 ) {
+    $mysqli = new mysqli('localhost', 'root', 'dwddwddwd', 'unit');
+    /* check connection */
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
 // get data and store in a json array
-$from = 0;
-$to = 4;
-$query = "SELECT CompanyName, ContactName, ContactTitle, Address, City FROM customers LIMIT ?,?";
-$result = $mysqli->prepare($query);
-$result->bind_param('ii', $from, $to);
-$result->execute();
-/* bind result variables */
-$result->bind_result($CompanyName, $ContactName, $ContactTitle, $Address, $City);
-/* fetch values */
-while ($result->fetch())
-{
-    $orders[] = array(
-        'CompanyName' => $CompanyName,
-        'ContactName' => $ContactName,
-        'ContactTitle' => $ContactTitle,
-        'Address' => $Address,
-        'City' => $City
-    );
+
+    $id = 1;
+    $query = "SELECT title ,request_date , attachment_request_link , id  FROM `units_requests`   where id_responder IS NOT NULL";
+
+
+    $result = $mysqli->prepare($query);
+    $result->execute();
+    $result->bind_result($title, $request_date, $attachment_request_link, $id);
+    /* fetch values */
+    while ($result->fetch()) {
+        $orders[] = array(
+            'title' => $title,
+            'request_date' => $request_date,
+            'attachment_request_link' => "<a href=$attachment_request_link> attachment request link</a>",
+            'update' => "<a href=update.php?id=$id> update this </a>",
+            'delete' => "<a href=delete.php?id=$id> delete  this </a>",
+            'reply'=>'reply'
+        );
+    }
+    echo json_encode($orders);
+    $result->close();
+    $mysqli->close();
 }
-echo json_encode($orders);
-/* close statement */
-$result->close();
-/* close connection */
-$mysqli->close();
