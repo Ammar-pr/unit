75101e446436c323474a240832a2d9344d2bdbf4
MSIGRENADY <cs.ammarn@gmail.com>
2017-11-01 21:33:23 -0800

    git commit 'only update left for admin user  '

--- grid_data.php
+++ grid_data.php
@@ -37,7 +37,7 @@ if(isset($_SESSION["username"]) & isset($_SESSION["password"]) & isset($_SESSION
             'title' => $title,
             'request_date' => $request_date,
             'attachment_request_link' => "<a href=$attachment_request_link> attachment request link</a>",
-            'update' => "<a href=update.php?id=$id> update this </a>",
+            'update' => "<a href=../update.php?id=$id> update this </a>",
             'delete' => "<a href=../deleting.php?id=$id> delete  this </a>",
             'reply'=>"<a href=../file_Upload.php?id=$id> reply  this </a>",
 // file_Upload
---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GRENADY
+ * Date: 01/11/17
+ * Time: 09:30 م
+ */
+
+class updating{
+
+
+
+    public 
+
+}
