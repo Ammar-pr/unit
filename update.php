3dfd92963ea4c0d0fe347f53ff286e04bfae011f
MSIGRENADY <cs.ammarn@gmail.com>
2017-11-01 21:52:14 -0800

    git commi 'trying to send the values of one request , using smarty to tpl page to show it to admin for updating  '

--- update.php
+++ update.php
@@ -5,11 +5,60 @@
  * Date: 01/11/17
  * Time: 09:30 م
  */
+session_start();
+date_default_timezone_set('America/New_York');
+require_once ('units_requests.php');
+require_once'smarty-master/libs/Smarty.class.php';
+$smarty = new Smarty();
+$smarty->template_dir = 'templates/';
+$smarty->compile_dir = 'templates_c/';
+$smarty->config_dir = 'configs/';
+$smarty->cache_dir = 'cache/';
+class updating
+{
 
-class updating{
+    public static function GET($value)
+    {
+        // $value=htmlspecialchars($value);
+        if ($value <= 0) {
+            return false;
+
+        } else {
+            if (is_numeric($value)) {
+
+                if (preg_match("@([^0-9])@Ui", $value) == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
 
 
+            }
+        }
+    }
 
-    public 
+    public function show_update_templates()
+    {
+        if (isset($_SESSION["username"]) & isset($_SESSION["password"]) & isset($_SESSION["authorization"])) {
 
+            if ($_SESSION["authorization"] == 22) {
+
+                $status = updating::GET(intval($_GET['id']));
+                if ($status) {
+
+                $unit= new units_requests();
+                 $value=   $unit->fetchWithPK(intval($_GET['id']));
+
+              foreach ($value as $v=>$k){
+                  echo $v=>$k;
+                    }
+            //   print_r($value);
+
+                }
+            }
+
+        }
+    }
 }
+$up=new updating();
+$up->show_update_templates ();
